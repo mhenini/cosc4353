f2ae7c6005a7559718573bb6439d5055b3680aba
Jeffry Sam <jeffrysam1999@hotmail.com>
2020-03-23 20:09:07 -0500

    Added Change Password Page

---
+++ cosc4353/Change_Password.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cosc4353
+{
+    public partial class Change_Password : System.Web.UI.Page
+    {
+        protected void ChangePassClick(object sender, System.EventArgs e)
+        {
+            String oldpass = OldPassword.Text;
+            String newpass = NewPassword1.Text;
+            String confirmpass = NewPassword2.Text;
+
+            if (newpass == confirmpass)
+            {
+                Correct.Text = "Password Changed Successfully";
+            }
+            else { Correct.Text = "Passwords do not match"; }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
--- cosc4353/History.aspx.cs
+++ cosc4353/History.aspx.cs
@@ -5,13 +5,18 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
+
 namespace cosc4353
 {
     public partial class History : System.Web.UI.Page
     {
+        
+        
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            String temp = "Just a test";
+            text1.Text = temp;
         }
     }
 }
